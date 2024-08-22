566f4a3089c8e6a97e8cb2885e68cfb0c1884ff1
MaiDoanKieuTrinh19 <maidoankieutrinh@gmail.com>
2024-08-22 14:49:15 +0700

    Add files via upload

---
+++ abc.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 22 14:34:57 2024
+
+@author: MaiDoanKieuTrinh-23732851
+"""
+a=int(input('Nhập a= '))
+b=int(input('Nhập b= '))
+c=int(input('Nhập c= '))
+print(f'{a}x^2 + {b}x + {c} = 0')
+
