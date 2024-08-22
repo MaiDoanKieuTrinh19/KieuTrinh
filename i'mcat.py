b3af5b01d9833448264dc484f3e60d79e7e34386
MaiDoanKieuTrinh19 <maidoankieutrinh@gmail.com>
2024-08-22 20:12:04 +0700

    Add files via upload

---
+++ i'mcat.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 22 19:54:57 2024
+
+@author: MaiDoanKieuTrinh-23732851
+"""
+
+a= "i'm a cat"
+print(a[0].upper() + a[1:3] + a[3:7].upper() + a[7:])
+print(a.upper())
+print(a[:1].lower() + a[1:3].upper() + a[3:7] + a[7:].upper())
+print(a.capitalize())
